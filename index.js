36e201dffc2dc120f284d116c356d960165c1fb9
John <john.kenwood88@gmail.com>
2023-11-26 17:29:33 -0800

    remove playerSelection function, add playRound and game functions

--- index.js
+++ index.js
@@ -1,20 +1,73 @@
 function getComputerChoice() {
     let num = Math.floor(Math.random() * 3) + 1;
     if (num === 1) {
-        return "Rock";
+        return "rock";
     } else if (num === 2) {
-        return "Paper";
+        return "paper";
     } else {
-        return "Scissors";
+        return "scissors";
+    }
+}
+ 
+function playRound(playerChoice, computerSelection) {
+    if (playerChoice === "rock" && computerSelection === "paper") {
+        return "You lose";
+    } else if (playerChoice === "rock" && computerSelection === "scissors") {
+        return "You win";
+    } else if (playerChoice === "paper" && computerSelection === "rock") {
+        return "You win";
+    } else if (playerChoice === "paper" && computerSelection === "scissors") {
+        return "You lose";
+    } else if (playerChoice === "scissors" && computerSelection === "rock") {
+        return "You lose";
+    } else if (playerChoice === "scissors" && computerSelection === "paper") {
+        return "You win";
+    } else {
+        return "Draw";
     }
 }
 
-function playerSelection() {
-   let choice = prompt("Throw!");
-   choice = choice.toLowerCase();
-   return choice;
+function game() {
+    
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        let playerChoice = prompt("Throw!");
+        playerChoice = playerChoice.toLowerCase();
+        let computerSelection = getComputerChoice();
+
+        let roundResult = playRound(playerChoice, computerSelection);
+        
+        console.log("Round: " + [i + 1]);
+        console.log("Computer: " + computerSelection);
+        console.log("You: " + playerChoice);
+        console.log(roundResult);
+        console.log("");
+
+        if (roundResult === "You lose") {
+            computerScore = computerScore + 1;
+        } else if (roundResult === "You win") {
+            playerScore = playerScore + 1;
+        } 
+    }
+
+    if (playerScore > computerScore) {
+        console.log("You WIN the game!");
+    } else if (playerScore < computerScore) {
+        console.log("You LOSE the game!");
+    } else {
+        console.log("The game is a draw");
+    }
+
+    console.log("Player: " + playerScore);
+    console.log("Computer: " + computerScore);
+    console.log("Thanks for throwing down!");
 }
 
-let computerSelection = getComputerChoice();
+
+
+
+
 
 
